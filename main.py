721778756538d9665dc8a5b3122fb7a3e4ac3a74
Tomasz Galecki <saleph@protonmail.com>
2019-12-28 18:27:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import networkx as nx
 import sys
 from initialize_simulation import initialize_simulation
+import graph_utils
+from factory_workflow import Workflow
 
 G = nx.DiGraph()
 G.add_edge(1, 2)
@@ -9,8 +11,17 @@ G.add_edge(3, 1)
 G.add_edge(3, 2)
 G.add_edge(1, 3)
 G.add_edge(2, 3)
-initialize_simulation(G)
-while True:
-    data = sys.stdin.read()
-    if data == 'x':
-        break
+
+#graph_utils.dump_geffi(G, "geffi.gmfx")
+#graph_utils.draw(graph_utils.load_geffi("geffi.gmfx"))
+#factory_workflow.dump_json(factory_workflow.parts, "factory_desc.json")
+
+workflow = Workflow("factory_desc.json")
+print(workflow)
+print(workflow.get_base_materials())
+print(workflow.get_ingredients('car'))
+#initialize_simulation(G)
+#while True:
+#    data = sys.stdin.read()
+#    if data == 'x':
+#        break
