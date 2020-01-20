03fedce88c3c18b6c1bf6091b959170f5e63a72d
piotr <myrepo7home>
2020-01-20 09:25:32 -0800

    Refactor

--- FactoryCreator.py
+++ FactoryCreator.py
@@ -31,22 +31,10 @@ class FactoryCreator:
         """
         agents_ids = self.graph.nodes()
         root_id = int()
-        storage_username = ""
-        agent_type_setter = {}
 
         for a_id in agents_ids:
             if str("car") in self.graph.nodes[a_id]["part"]:
                 root_id = a_id
-                agent_type_setter[a_id] = AgentType.CAR
-            elif str("wheel") in self.graph.nodes[a_id]["part"]:
-                agent_type_setter[a_id] = AgentType.WHEEL
-            elif str("door") in self.graph.nodes[a_id]["part"]:
-                agent_type_setter[a_id] = AgentType.DOOR
-            elif str("engine") in self.graph.nodes[a_id]["part"]:
-                agent_type_setter[a_id] = AgentType.ENGINE
-            else:
-                agent_type_setter[a_id] = AgentType.STORAGE
-                storage_username = "agent_" + str(a_id) + "@localhost"
 
         neighbours = dict([
             (agent_id, dict(
@@ -55,7 +43,7 @@ class FactoryCreator:
             )
              ) for agent_id in agents_ids
         ])
-        self._initialize_agents(agents_ids, neighbours, storage_username, agent_type_setter, hostname=self.hostname)
+        self._initialize_agents(agents_ids, neighbours, hostname=self.hostname)
         initialize_logger()
         for agent in self.agents.values():
             agent.start()
@@ -64,31 +52,44 @@ class FactoryCreator:
 
         return self.agents
 
-    def _initialize_agents(self, agent_ids, neighbours_lists, storage_username, agent_type_setter,
+    def _initialize_agents(self, agent_ids, neighbours_lists,
                            basename="agent", hostname="localhost"):
         self.agent_usernames = dict([(agent_id, "{}_{}@{}".format(basename, agent_id, hostname))
-                                for agent_id in agent_ids])
+                                     for agent_id in agent_ids])
         agent_username_to_id = {v: k for k, v in self.agent_usernames.items()}
         AgentUsernameToIdMapper.agent_username_to_id = agent_username_to_id
 
         self.full_neighbours_map = dict([
             (self.agent_usernames[agent_id], dict(
-                successors=[self.agent_usernames[neighbour_id] for neighbour_id in neighbours_lists[agent_id]['successors']],
+                successors=[self.agent_usernames[neighbour_id] for neighbour_id in
+                            neighbours_lists[agent_id]['successors']],
                 predecessors=[self.agent_usernames[neighbour_id] for neighbour_id in
                               neighbours_lists[agent_id]['predecessors']]
             )
              ) for agent_id in agent_ids])
 
         for agent_id in agent_ids:
-            self.create_agent(agent_id, storage_username, agent_type_setter)
+            self.create_agent(agent_id)
 
-
-    def create_agent(self, agent_id, storage_username, agent_type_setter, respawn_after_breakdown = False):
+    def create_agent(self, agent_id, respawn_after_breakdown=False):
         username = self.agent_usernames[agent_id]
 
+        if str("car") in self.graph.nodes[agent_id]["part"]:
+            agent_type = AgentType.CAR
+        elif str("wheel") in self.graph.nodes[agent_id]["part"]:
+            agent_type = AgentType.WHEEL
+        elif str("door") in self.graph.nodes[agent_id]["part"]:
+            agent_type = AgentType.DOOR
+        elif str("engine") in self.graph.nodes[agent_id]["part"]:
+            agent_type = AgentType.ENGINE
+        else:
+            agent_type = AgentType.STORAGE
+
+        # TODO 69 from workflow
+        storage_username = "agent_" + str(69) + "@localhost"
+
         agent = FactoryAgent(username, username, factory_creator=self, storage_username=storage_username,
-                neighbours=self.full_neighbours_map[username], agent_type=agent_type_setter.get(agent_id))
+                             neighbours=self.full_neighbours_map[username], agent_type=agent_type)
         self.agents[agent_id] = agent
         self.agents[agent_id].respawn_after_breakdown = respawn_after_breakdown;
         return agent
-
